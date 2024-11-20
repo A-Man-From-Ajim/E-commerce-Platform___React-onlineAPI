895c38a15ce3630d9d3244b83b7d06aad81324a0
aManFromJirba <ahmed.lachhab020@gmail.com>
2024-11-20 09:47:58 +0100

    fef

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,12 +10,6 @@ const Home = ({ updateCart }) => {
     useEffect(() => {
         const fetchProducts = async () => {
             try {
-                // const response = await fetch(
-                //     `${process.env.REACT_APP_API_URL}/api/products`
-                // );
-                // const data = await response.json();
-                // setProducts(data);
-                // setFilteredproducts(data);
                 const response = await fetch(
                     `https://fakestoreapi.com/products`
                 );
--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -3,20 +3,12 @@ import { Stack } from 'react-bootstrap';
 import { useEffect, useState } from "react";
 export default function ProductDetail() {
     const { id } = useParams();
-
-
     const [product, setProduct] = useState({})
     const [loading, setLoading] = useState(true);
-
 
     useEffect(() => {
         const fetchProduct = async () => {
             try {
-                // const response = await fetch(
-                //     `${process.env.REACT_APP_API_URL}/api/products/${id}`
-                // );
-                // const data = await response.json();
-                // setProduct(data);
                 const response = await fetch(
                     `https://fakestoreapi.com/products/${id}`
                 );
@@ -29,12 +21,12 @@ export default function ProductDetail() {
             }
         };
         fetchProduct();
-    })
+    }, [])
 
     if (loading) return <h1>...Loading</h1>
 
     return <>
-        <Stack direction='horizontal' gap={2}>
+        <div >
             <div className="p-2 flex justify-center" >
                 <img src={`${product.image}`} alt="" height={5} width={100} />
             </div>
@@ -43,9 +35,6 @@ export default function ProductDetail() {
                 <p>{product.description}</p>
             </div>
 
-            <div className="p-2">
-            </div>
-
-        </Stack>
+        </div>
     </>;
 }
